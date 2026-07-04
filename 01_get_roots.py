4c2b10a164e82cd74a78d39c351a8cf8a3699348
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-07-04 07:51:12 -0400

    Refactor and rerun analysis (#1)

--- 01_get_roots.py
+++ 01_get_roots.py
@@ -1,21 +1,52 @@
-"""Get roots that are annotated with IAO_0000700."""
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "bioontologies>=0.8.3",
+#     "bioregistry>=0.13.64",
+#     "click>=8.4.2",
+#     "curies",
+#     "curies-processing",
+#     "more-click>=0.1.3",
+#     "pandas>=3.0.3",
+#     "robot-obo-tool>=0.0.1",
+#     "tqdm>=4.68.3",
+#     "pystow>=0.8.21",
+# ]
+#
+# [tool.uv.sources]
+# bioregistry = { path = "../bioregistry" }
+# curies = { path = "../curies" }
+# curies-processing = { path = "../curies-processing" }
+# bioontologies = { path = "../bioontologies" }
+# ///
+
+"""Get roots that are annotated with IAO:0000700."""
 
 import json
 from pathlib import Path
-
+import obographs
 import bioontologies
 import bioregistry
 import click
+from robot_obo_tool import ROBOTError
+from tqdm.contrib.logging import logging_redirect_tqdm
 import pandas as pd
-from more_click import verbose_option
+from more_click import verbose_option, force_option
 from tqdm import tqdm
+from pystow.utils import write_pydantic_json
 
 HERE = Path(__file__).parent.resolve()
 RESULTS_TSV_PATH = HERE.joinpath("results.tsv")
 RESULTS_JSON_PATH = HERE.joinpath("results.json")
 ERRORS_PATH = HERE.joinpath("errors.tsv")
 
-CANONICAL = {
+CACHE_DIR = HERE.joinpath("docs", "cache")
+CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
+#: manual overrides for when the graph's IRI is not clearly inferrable by the prefix
+CANONICAL_GRAPH_IRI = {
     "cheminf": "http://semanticchemistry.github.io/semanticchemistry/ontology/cheminf.owl",
     "dideo": "http://purl.obolibrary.org/obo/dideo/release/2022-06-14/dideo.owl",
     "micro": "http://purl.obolibrary.org/obo/MicrO.owl",
@@ -23,9 +54,15 @@ CANONICAL = {
     "mfomd": "http://purl.obolibrary.org/obo/MF.owl",
     "one": "http://purl.obolibrary.org/obo/ONE",
     "ons": "https://raw.githubusercontent.com/enpadasi/Ontology-for-Nutritional-Studies/master/ons.owl",
+    "geogeo": "http://purl.obolibrary.org/obo/geo.owl",
 }
 SKIP = {
-    "ncbitaxon",
+    "ncbitaxon",# big
+    "ncit", #big
+    "chebi", # big
+    "gaz", # big
+    "epio", # garb
+    "interpro", # irrelevant
 }
 NO_ROOTS_MSG = "no roots annotated with IAO_0000700"
 DOMAINS = {
@@ -35,45 +72,62 @@ DOMAINS = {
 
 
 @click.command()
+@force_option
 @verbose_option
-def main():
-    errors = pd.read_csv(ERRORS_PATH, sep="\t")
-    error_prefixes = set(errors[errors["message"] == NO_ROOTS_MSG].prefix)
+def main(force: bool) -> None:
+    if ERRORS_PATH.is_file() and not force:
+        errors = pd.read_csv(ERRORS_PATH, sep="\t")
+        error_prefixes = set(errors[errors["message"] == NO_ROOTS_MSG].prefix)
+    else:
+        error_prefixes = set()
 
-    roots = json.loads(RESULTS_JSON_PATH.read_text())
-    prefixes = [
-        (resource.prefix, resource.get_obofoundry_prefix())
+    if RESULTS_JSON_PATH.is_file() and not force:
+        prefix_to_roots = json.loads(RESULTS_JSON_PATH.read_text())
+    else:
+        prefix_to_roots = {}
+
+    prefixes: list[tuple[str, str]] = [
+        (resource.prefix, resource, obo_prefix)
         for resource in bioregistry.resources()
         if (
-            resource.get_obofoundry_prefix()
+            (obo_prefix := resource.get_obofoundry_prefix())
             and not resource.is_deprecated()
-            and resource.prefix not in roots
+            and resource.prefix not in prefix_to_roots
             and resource.prefix not in SKIP
-            and resource.obofoundry["domain"] in DOMAINS
+            # and resource.obofoundry["domain"] in DOMAINS
         )
     ]
     missing = []
-    it = tqdm(prefixes, unit="ontology")
-    for prefix, obo_prefix in it:
+    it = tqdm(prefixes, unit="ontology", desc="Extracting roots")
+    for prefix, resource, obo_prefix in it:
         it.set_postfix(prefix=obo_prefix)
 
         if prefix in error_prefixes:
-            missing.append((prefix, NO_ROOTS_MSG))
+            missing.append((prefix, obo_prefix,
+                    resource.get_name(),
+                    resource.get_repository(),NO_ROOTS_MSG))
             continue
 
         try:
-            parse_results = bioontologies.get_obograph_by_prefix(prefix)
-        except TypeError:
+            with logging_redirect_tqdm():
+                parse_results = bioontologies.get_obograph_by_prefix(
+                    prefix, check=False
+                )
+        except (TypeError, ROBOTError):
             tqdm.write(f"[{prefix}] malformed data")
-            missing.append((prefix, "malformed data"))
+            missing.append((prefix, obo_prefix,
+                    resource.get_name(),
+                    resource.get_repository(),"malformed data"))
             continue
 
         if not parse_results.graph_document:
             tqdm.write(f"[{prefix}] no graph document")
-            missing.append((prefix, "no document"))
+            missing.append((prefix,obo_prefix,
+                    resource.get_name(),
+                    resource.get_repository(), "no document"))
             continue
 
-        graphs = parse_results.graph_document.graphs
+        graphs: list[obographs.Graph] = parse_results.graph_document.graphs
         if 1 == len(graphs):
             graph = graphs[0]
         else:
@@ -81,8 +135,11 @@ def main():
             standard_id = f"http://purl.obolibrary.org/obo/{obo_prefix.lower()}.owl"
             if standard_id in id_to_graph:
                 graph = id_to_graph[standard_id]
-            elif prefix in CANONICAL and CANONICAL[prefix] in id_to_graph:
-                graph = id_to_graph[CANONICAL[prefix]]
+            elif (
+                prefix in CANONICAL_GRAPH_IRI
+                and CANONICAL_GRAPH_IRI[prefix] in id_to_graph
+            ):
+                graph = id_to_graph[CANONICAL_GRAPH_IRI[prefix]]
             else:
                 tqdm.write(f"[{prefix}] has multiple graphs:")
                 for i, graph in enumerate(graphs):
@@ -90,24 +147,29 @@ def main():
                 missing.append((prefix, "multiple graphs"))
                 continue
 
+        write_pydantic_json(graph, CACHE_DIR.joinpath(prefix).with_suffix(".json"), indent=2, exclude_unset=True, exclude_none=True, exclude_defaults=True)
+
         if not graph.roots:
-            missing.append((prefix, NO_ROOTS_MSG))
+            missing.append(
+                (
+                    prefix,
+                    obo_prefix,
+                    resource.get_name(),
+                    resource.get_repository(),
+                    NO_ROOTS_MSG,
+                )
+            )
             continue
 
-        labels = {
-            node.id: node.lbl
-            for node in tqdm(
-                graph.nodes,
-                desc=f"caching {prefix} labels",
-                unit="term",
-                unit_scale=True,
-            )
-            if node.lbl
+        node_uri_to_label = {node.id: node.lbl for node in graph.nodes if node.lbl}
+        prefix_to_roots[prefix] = {
+            root: node_uri_to_label.get(root) for root in graph.roots
         }
-        roots[prefix] = {root: labels.get(root) for root in graph.roots}
 
     # make outputs on all rows
-    errors_df = pd.DataFrame(missing, columns=["prefix", "message"])
+    errors_df = pd.DataFrame(
+        missing, columns=["prefix", "obo", "name", "repository", "message"]
+    )
     errors_df.to_csv(ERRORS_PATH, sep="\t", index=False)
 
     roots_rows = [
@@ -116,13 +178,13 @@ def main():
             root.removeprefix("http://purl.obolibrary.org/obo/").replace("_", ":"),
             root_label,
         )
-        for prefix, data in sorted(roots.items())
-        for root, root_label in sorted(data.items())
+        for prefix, roots in sorted(prefix_to_roots.items())
+        for root, root_label in sorted(roots.items())
     ]
     results_df = pd.DataFrame(roots_rows, columns=["prefix", "root", "label"])
     results_df.to_csv(RESULTS_TSV_PATH, sep="\t", index=False)
 
-    RESULTS_JSON_PATH.write_text(json.dumps(roots, sort_keys=True, indent=2))
+    RESULTS_JSON_PATH.write_text(json.dumps(prefix_to_roots, sort_keys=True, indent=2))
 
 
 if __name__ == "__main__":
--- 03_make_table.py
+++ 03_make_table.py
@@ -1,13 +1,29 @@
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "bioregistry>=0.13.64",
+#     "pandas>=3.0.3",
+#     "tabulate>=0.10.0",
+# ]
+# ///
+
 import bioregistry
 import pandas
+import click
 from tabulate import tabulate
 
-if __name__ == "__main__":
+
+@click.command()
+def main() -> None:
     df = pandas.read_csv("errors.tsv", sep="\t")
     df = df[df["message"] == "no roots annotated with IAO_0000700"]
     rows = []
     for prefix in df.prefix:
-        resource = bioregistry.get_resource(prefix)
+        resource = bioregistry.get_resource(prefix, strict=True)
+        if resource.obofoundry is None:
+            raise
         domain = resource.obofoundry["domain"]
         if domain in {"phenotype", "anatomy and development"}:
             rows.append(
@@ -15,8 +31,16 @@ if __name__ == "__main__":
                     domain,
                     f"[{prefix}](https://obofoundry.org/ontology/{prefix})",
                     resource.get_repository(),
-                    f"![](https://cthoyt.com/cob-root-analysis/results/{prefix}_results.svg)"
+                    f"![](https://cthoyt.com/cob-root-analysis/results/{prefix}_results.svg)",
                 )
             )
     rows = sorted(rows)
-    print(tabulate(rows, headers=["domain", "prefix", "repository", "image"], tablefmt="github"))
+    click.echo(
+        tabulate(
+            rows, headers=["domain", "prefix", "repository", "image"], tablefmt="github"
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
